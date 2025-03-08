60e3d28931d6239a77418207ec29b58c1652cf37
MrAmfix <nat.rud.ufa@gmail.com>
2025-03-09 01:33:30 +0300

    Removed black-formatter

--- database/gen_session.py
+++ database/gen_session.py
@@ -3,10 +3,14 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from bot.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_IP, POSTGRES_PORT, POSTGRES_DB
 
 
-DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_IP}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = (f"postgresql+asyncpg://"
+                f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+                f"{POSTGRES_IP}:{POSTGRES_PORT}/{POSTGRES_DB}")
 engine = create_async_engine(DATABASE_URL, future=True)
-SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, autocommit=False,
-                                  autoflush=False, class_=AsyncSession, future=True)
+SessionLocal = async_sessionmaker(
+    bind=engine, expire_on_commit=False, autocommit=False,
+    autoflush=False, class_=AsyncSession, future=True
+)
 
 
 async def get_session() -> Generator:
